911bf34af6cc4fc2e56d23b98adfcb8c6dd167c8
Adane Moges <adanemogs6@gmail.com>
2023-08-02 02:47:04 +0300

    advance

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-from = ARGV[0][/from:(\S+)/, 1]
-to = ARGV[0][/from:(\S+)/, 1]
-flag = ARGV[0][/flags:(\S+)/, 1]
+from = ARGV[0][/from:(\S+)\]/, 1]
+to = ARGV[0][/to:(\S+)\]/, 1]
+flag = ARGV[0][/flags:(\S+)\]/, 1]
 
 puts "#{from},#{to},#{flag}"
