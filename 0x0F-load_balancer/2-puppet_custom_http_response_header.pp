048d1d39502373402bd611c0fe39d083708746b4
Adane <adanemoges6@gmail.com>
2023-10-03 03:22:35 +0300

    the puppet file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+exec { 'update':
+  command => 'usr/bin/apt-get -y update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update']
+}
+file_line { 'redirect':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-avaliable/default',
+  after   => 'server_name_;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require =>Package['nginx'],
+}
+file_line { 'heade':
+  ensure  => 'present',
+  path    => '/etc/nginx/nginx.conf',
+  after   =>'sendfile on;',
+  line    => 'add_header X-Served-By "$HOSTNAME";',
+  require => Pakage['nginx'],
+}
+file{ '/var/www/html/index.html':
+  content => 'hello world',
+  require => Package['nginx'],
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
