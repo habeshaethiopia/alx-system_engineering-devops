e93f623a1cc4fb2f07113c8818615cf7171407a8
Adane <adanemoges6@gmail.com>
2023-10-24 00:36:39 +0300

    first git

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""INtro to restfulapi"""
+import requests
+import urllib
+from sys import argv
+import json
+
+result = []
+count = 0
+response = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+response2 = json.loads(
+    (
+        urllib.request.urlopen(
+            f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+        .read()
+        .decode("utf-8")
+    )
+)
+for i in response:
+    if i.get("userId") == int(argv[1]):
+        result.append(i)
+        if i.get("completed") is True:
+            count += 1
+print(
+    f"Employee {response2.get('name')} is done with tasks\
+    ({count}/{len(result)})")
+for i in result:
+    if i["completed"] is True:
+        print("\t {}".format(i["title"]))
