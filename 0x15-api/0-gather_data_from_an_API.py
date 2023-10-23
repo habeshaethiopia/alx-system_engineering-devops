f1c467b899abd17bbb28d6d3dafe66e6f118aed6
Adane <adanemoges6@gmail.com>
2023-10-24 00:41:15 +0300

    file should not be executed when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,25 +5,29 @@ import urllib
 from sys import argv
 import json
 
-result = []
-count = 0
-response = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
-response2 = json.loads(
-    (
-        urllib.request.urlopen(
-            f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
-        .read()
-        .decode("utf-8")
+
+if __name__ == "__main__":
+    result = []
+    count = 0
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+    response2 = json.loads(
+        (
+            urllib.request.urlopen(
+                f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+            )
+            .read()
+            .decode("utf-8")
+        )
     )
-)
-for i in response:
-    if i.get("userId") == int(argv[1]):
-        result.append(i)
+    for i in response:
+        if i.get("userId") == int(argv[1]):
+            result.append(i)
+            if i.get("completed") is True:
+                count += 1
+    print(
+        f"Employee {response2.get('name')} is done with tasks\
+        ({count}/{len(result)})"
+    )
+    for i in result:
         if i.get("completed") is True:
-            count += 1
-print(
-    f"Employee {response2.get('name')} is done with tasks\
-    ({count}/{len(result)})")
-for i in result:
-    if i.get("completed") is True:
-        print("\t {}".format(i.get("title")))
+            print("\t {}".format(i.get("title")))
