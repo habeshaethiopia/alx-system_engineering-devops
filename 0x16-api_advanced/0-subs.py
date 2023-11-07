ef0ddc4043bcb69f15a2fa8ea42dc64e0c76f86b
Adane <adanemoges6@gmail.com>
2023-11-07 22:04:30 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        return r.json().get("data").get("subscribers")
+    else:
+        return 0
