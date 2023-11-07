563e183e43af2084ad326c774ee42e297fbe010f
Adane <adanemoges6@gmail.com>
2023-11-07 22:25:34 +0300

    Add function to retrieve top ten posts from a subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Top Ten"""
+import requests
+def top_ten(subreddit):
+    """the top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    r = requests.get(url, headers=headers, params={"limit": 10})
+    if r.status_code == 200:
+        for post in r.json().get("data").get("children"):
+            print(post.get("data").get("title"))
+    else:
+        print(None)
