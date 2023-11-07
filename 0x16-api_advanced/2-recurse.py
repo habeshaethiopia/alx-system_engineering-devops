6f0d66ca91899d01fd0f0b5467e9bbf40bb1f429
Adane <adanemoges6@gmail.com>
2023-11-07 22:36:06 +0300

    Add recursive function to retrieve hot posts from a subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""the recursive function"""
+import requests
+def recurse(subreddit, hot_list=[]):
+    """recursive function"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    r = requests.get(url, headers=headers, params={"limit": 100})
+    if r.status_code == 200:
+        for post in r.json().get("data").get("children"):
+            hot_list.append(post.get("data").get("title"))
+        after = r.json().get("data").get("after")
+        if after is not None:
+            return recurse(subreddit, hot_list)
+        else:
+            return hot_list
+    else:
+        return None
